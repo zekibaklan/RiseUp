eebf2f50cd19848db20d69c1be0b5b837e939e63
Zeki Baklan <zekibaklan@gmail.com>
2024-04-02 14:30:02 +0300

    Initial Commit

---
+++ RiseUp/RiseUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseUpApp.swift
+//  RiseUp
+//
+//  Created by Zeki Baklan on 2.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RiseUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
